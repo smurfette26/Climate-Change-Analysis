a4d8e1291283f878fcfa3b0a0ee79837acdfabe0
smurfette26 <159061640+smurfette26@users.noreply.github.com>
2024-03-31 14:50:52 +0000

    file upload chnages

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for, template_rendered
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
+#app.config['UPLOAD_FOLDER'] = '/static/uploads'
+app.config['UPLOAD_PATH'] = 'uploads'
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 
 
 @app.route("/preprocessing")
 def preprocessing():
   data = {}
   return render_template('pre_processing.html', context=data)
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+  if request.method == 'POST':
+    f = request.files['file']
+    #f.save(secure_filename(f.filename))
+    filename = secure_filename(f.filename)
+    print(filename)
+    f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return redirect(url_for('preprocessing'))
+  else:
+    return redirect(url_for('myApp'))
 
 
 @app.route("/")
