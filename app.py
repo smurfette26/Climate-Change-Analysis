d9b92a21f22573cc6d510b2ca85e7a3605fa6178
smurfette26 <159061640+smurfette26@users.noreply.github.com>
2024-03-31 15:12:04 +0000

    redirected page and collab libraries imports

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for, template_rendered
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
+
+####Data processing libraries
+from pandas import read_csv, DataFrame
+from statistics import mean, stdev
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from statistics import mode, mean, median, stdev, variance
+from collections import Counter
+from statistics import variance, stdev
+from numpy import  transpose
+import numpy as np
+
 
 app = Flask(__name__)
 
@@ -10,8 +20,13 @@ app.config['UPLOAD_PATH'] = 'uploads'
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 
 
-@app.route("/preprocessing")
-def preprocessing():
+def data_processing(filename):
+  
+  
+
+@app.route("/preprocessing/<filename>")
+def preprocessing(filename):
+  print(filename)
   data = {}
   return render_template('pre_processing.html', context=data)
 
@@ -22,9 +37,8 @@ def upload_file():
     f = request.files['file']
     #f.save(secure_filename(f.filename))
     filename = secure_filename(f.filename)
-    print(filename)
     f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-    return redirect(url_for('preprocessing'))
+    return redirect(url_for('preprocessing', filename=filename))
   else:
     return redirect(url_for('myApp'))
 
